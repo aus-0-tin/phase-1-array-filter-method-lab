75b5f94e241407059aad9f1c70cd22b2adcfec5a
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-03-29 20:19:13 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+
+  // Function to find drivers whose names begin with the provided letters
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+
+  // Function to match driver names with the provided string
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
